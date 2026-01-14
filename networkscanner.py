6bacb5731f32e8229ec449370a1f9648e84f578d
ThcReaper <ThcReaper@icloud.com>
2026-01-14 11:34:20 -0500

    Add files via upload

---
+++ networkscanner.py
@@ -0,0 +1,32 @@
+from scapy.all import ARP, Ether, srp
+from scapy.all import*
+conf.use_pcap
+
+#If you are on windows you will need to go to this website and download npcap
+# https://npcap.com/#download
+target_ip = "192.168.1.1/24"
+
+
+arp = ARP(pdst=target_ip)
+
+
+
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+# stack them
+
+packet = ether/arp
+
+result =srp(packet, timeout = 3)[0]
+
+clients = []
+
+
+for sent,received in result:
+	# for each response, append ip andmac address to clients lists
+	clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+print('Available devicesin the network:')
+print("IP" + " "*18+"MAC")
+for client in clients:
+	print("{:16} {}".format(client['ip'], client['mac']))
